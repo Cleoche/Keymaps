4bc273900786102241562f98991259b316b38b58
Cléo Thor <kiszo.r6@gmail.com>
2024-03-15 19:52:29 -0400

    new stable cu7 keymap

--- capsunlocked/cu7/keymaps/stable/keymap.c
+++ capsunlocked/cu7/keymaps/stable/keymap.c
@@ -17,6 +17,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 #define _MEDIA 0
 #define _DEV 1
+#define _LAYERS 2
+#define _DISCORD 3
+#define _LRFLAG 4
+#define _LRRATE 5
 
 
 
@@ -25,14 +29,34 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_MEDIA] = LAYOUT(
              KC_MPLY,
-    LSA_T(KC_MPRV),    MO(_DEV),    KC_MNXT,
-    KC_MUTE,    XXXXXXX,    KC_SYSTEM_POWER
+    LSA_T(KC_MPRV),    TG(_LAYERS),    KC_MNXT,
+    LT(_DEV, KC_MUTE),    XXXXXXX,    KC_SYSTEM_POWER
   ),
   [_DEV] = LAYOUT(
              QK_BOOT,
-    RGB_HUI,    _______,    RGB_HUD,
-    RGB_SAI,    TO(_MEDIA),    RGB_SAD
-  ) 
+    _______,    RGB_TOG,    _______,
+    _______,    TO(_MEDIA),    _______
+  ),
+  [_LAYERS] = LAYOUT(
+             TO(_DEV),
+    TO(_DISCORD),    _______,    _______,
+    TO(_LRFLAG),    TO(_MEDIA),    _______
+  ),
+  [_DISCORD] = LAYOUT(
+             LCTL(KC_F17),
+    KC_F18,    KC_F19,    KC_F17,
+    LCTL(KC_LBRC),    TO(_MEDIA),    LCTL(KC_RBRC)
+  ),
+  [_LRFLAG] = LAYOUT(
+             XXXXXXX,
+    KC_X,    KC_U,    KC_P,
+    MO(_LRRATE),    TO(_MEDIA),    XXXXXXX
+  ),
+  [_LRRATE] = LAYOUT(
+             KC_0,
+    KC_1,    KC_2,    KC_3,
+    _______,    KC_4,    KC_5
+  )
 };
 
 // Volume up/down on the encoder
@@ -40,15 +64,27 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
   uint8_t current_layer = get_highest_layer(layer_state);
   if (current_layer == _DEV) {
     if (clockwise) {
-      tap_code_delay(KC_PAUSE, 10);
+      tap_code_delay(KC_BRIU, 10);
     } else {
-      tap_code_delay(KC_SCROLL_LOCK, 10);
+      tap_code_delay(KC_BRID, 10);
+    }
+  } else if (current_layer == _LRFLAG) {
+    if (clockwise) {
+      tap_code_delay(KC_RGHT, 20);
+    } else {
+      tap_code_delay(KC_LEFT, 20);
+    }
+  } else if (current_layer == _DISCORD) {
+    if (clockwise) {
+      tap_code_delay(KC_MNXT, 20);
+    } else {
+      tap_code_delay(KC_MPRV, 20);
     }
   } else {
     if (clockwise) {
-      tap_code(KC_KB_VOLUME_UP);
+      tap_code(KC_VOLU);
     } else {
-      tap_code(KC_KB_VOLUME_DOWN);
+      tap_code(KC_VOLD);
     }
   } return false;
 }
